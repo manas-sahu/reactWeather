0c4b5c32e20f700f5f5907e840f090c9c6cc3aa9
manas.sahu.one@gmail.com <manas.sahu.one@gmail.com>
2020-03-14 00:51:51 +0530

    deploy

---
+++ precache-manifest.904161e761d43f0a671bc77f3a3973fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce21743d278fe87193c1d3d256f11517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "862d442442dd3d33eadb",
+    "url": "/static/css/main.53588fc9.chunk.css"
+  },
+  {
+    "revision": "6d057f808824e124aecb",
+    "url": "/static/js/2.47b3f608.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.47b3f608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "862d442442dd3d33eadb",
+    "url": "/static/js/main.949a2e15.chunk.js"
+  },
+  {
+    "revision": "f701792ed85e5f895166",
+    "url": "/static/js/runtime-main.cdfb4480.js"
+  }
+]);
